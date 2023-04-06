5c1577cb80fcc14151651a84c1b9fce480743c41
cthutqc <toserg81@mail.ru>
2023-04-06 12:39:17 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,10 +48,7 @@ class User extends Authenticatable implements FilamentUser, HasMedia
 
     public function canAccessFilament(): bool
     {
-        if($this->hasRole('admin'))
-            return true;
-        else
-            return false;
+        return $this->hasRole('admin');
     }
 
     public function items():HasMany
